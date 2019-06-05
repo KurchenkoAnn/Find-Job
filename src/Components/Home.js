04aabec6b4905272325ad593924e9f360d841cfc
Anonymous <user@rivne.itstep.org>
2019-06-05 17:54:41 +0300

    first commit

---
+++ src/Components/Home.js
@@ -0,0 +1,15 @@
+import React, { PureComponent } from 'react';   
+import NavMenu from './NavMenu';
+
+class Home extends Component {
+    state = {  }
+    render() { 
+        return ( 
+            <div>
+                <h1>Hello</h1>
+            </div>
+         );
+    }
+}
+ 
+export default Home;
